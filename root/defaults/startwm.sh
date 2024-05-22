1d8d8def666d1e8b2f404ba1e97c93d6db899282
thelamer <ryankuba@gmail.com>
2024-05-22 12:30:59 -0400

    add logic for injecting env vars on nvidia detection on non working Alpine branch along with docs

--- root/defaults/startwm.sh
+++ root/defaults/startwm.sh
@@ -1,2 +1,10 @@
 #!/usr/bin/env bash
+
+# Enable Nvidia GPU support if detected
+if vulkaninfo >/dev/null 2>&1 && which nvidia-smi; then
+  printf "1" > /run/s6/container_environment/LIBGL_KOPPER_DRI2
+  printf "zink" > /run/s6/container_environment/MESA_LOADER_DRIVER_OVERRIDE
+  printf "zink" > /run/s6/container_environment/GALLIUM_DRIVER
+fi
+
 /usr/bin/openbox-session
