c8a520dea990966c0e10be0b8307d197420cfe3c
thelamer <ryankuba@gmail.com>
2024-05-23 08:12:29 -0400

    pull out env vars on alpine

--- root/defaults/startwm.sh
+++ root/defaults/startwm.sh
@@ -1,10 +1,3 @@
 #!/usr/bin/env bash
 
-# Enable Nvidia GPU support if detected
-if which nvidia-smi; then
-  export LIBGL_KOPPER_DRI2=1
-  export MESA_LOADER_DRIVER_OVERRIDE=zink
-  export GALLIUM_DRIVER=zink
-fi
-
 /usr/bin/openbox-session
